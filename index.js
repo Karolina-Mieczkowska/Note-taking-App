3a894048379f5fcb70134f8af14b9d0b029be462
Karolina <kmieczkowska75@gmail.com>
2021-10-24 14:51:50 +0100

    Refactor and add cancel btn

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const footerDate = document.querySelector('.footer__date');
 const textArea = document.querySelector('.create__note--textarea');
-const btnAdd = document.querySelector('.btn__add');
-const btnSave = document.querySelector('.btn__save');
+const noteBoard = document.querySelector('.note__board');
+
 const mainCreateNote = document.querySelector('.main__create-note');
 const mainNoteBoard = document.querySelector('.main__note-board');
-const noteBoard = document.querySelector('.note__board');
-const btnDelete = document.querySelector('.btn__delete');
 
+const btnAdd = document.querySelector('.btn__add');
+const btnSave = document.querySelector('.btn__save');
+const btnDelete = document.querySelector('.btn__delete');
+const btnCancel = document.querySelector('.btn__cancel');
+
+const footerDate = document.querySelector('.footer__date');
 
 noteBoard.innerHTML = '';
 const notes = [];
 
 let noteEdited = false;
 let editedNoteIndex;
+
+// FUNCTIONS
+
+const openNoteArea = function() {
+    // Close note board
+    mainNoteBoard.style.display = 'none';
+    // Open note area
+    mainCreateNote.style.display = 'block';
+    // Show delete button
+    btnDelete.style.visibility = 'visible';
+    // Show cancel button
+    btnCancel.style.visibility = 'visible';
+}
+
+const closeNoteArea = function() {
+    // Close note area
+    mainCreateNote.style.display = 'none';
+    // Open note board
+    mainNoteBoard.style.display = 'block';
+    // Hide delete button
+    btnDelete.style.visibility = 'hidden';
+    // Hide cancel button 
+    btnCancel.style.visibility = 'hidden';
+}
 
 const updateUI = function(allNotes) {
 
@@ -35,60 +62,35 @@ const updateUI = function(allNotes) {
         pinnedNote.addEventListener('click', function() {
             editNote(ind);
             editedNoteIndex = ind;
-            console.log('click');
         })
     })
 }
 
-// STEP THREE
-
 const editNote = function(index) {
 
-    // Close note board
-    mainNoteBoard.style.display = 'none';
-    // Open note area
-    mainCreateNote.style.display = 'block';
-    // Show delete button
-    btnDelete.style.visibility = 'visible';
+    openNoteArea();
 
     textArea.value = notes[index]
 
     noteEdited = true;
 }
 
-// STEP FOUR
-
 const pinEditedNote = function(editedContent) {
-    notes[editedNoteIndex] = editedContent;
-    console.log(notes[editedNoteIndex]);
     
-    // Delete edited note
-
-    // Pin new note
-
-    // update UI
-    // notes = ['Ptak', 'Gąska', 'Lisek']
-    // updateUI(notes)
+    notes[editedNoteIndex] = editedContent;
 
     updateUI(notes);
 
     noteEdited = false;
 }
 
-// STEP ONE
-
 let noteContent;
 
-btnAdd.addEventListener('click', function() {
-    // Close note board
-    mainNoteBoard.style.display = 'none';
-    // Open note area
-    mainCreateNote.style.display = 'block';
-    // Show delete button
-    btnDelete.style.visibility = 'visible';
-})
+// BUTTONS
 
-// STEP TWO
+btnAdd.addEventListener('click', function() {
+    openNoteArea();
+})
 
 btnSave.addEventListener('click', function(ev) {
     ev.preventDefault();
@@ -97,9 +99,6 @@ btnSave.addEventListener('click', function(ev) {
     noteContent = textArea.value;
 
     // Pin note to the board
-
-    // updateUI(notes);
-
     if (!noteEdited) {
         // Add note
         notes.push(noteContent);
@@ -108,49 +107,28 @@ btnSave.addEventListener('click', function(ev) {
         pinEditedNote(noteContent);
     }
 
-    // const pinnedNotes = document.querySelectorAll('.note');
-
-    // pinnedNotes.forEach(function(pinnedNote, ind) {
-    //     pinnedNote.addEventListener('click', function() {
-    //         editNote(ind);
-    //         editedNoteIndex = ind;
-    //         console.log('click');
-    //     })
-    // })
-
     // Clear text area
     textArea.value = '';
 
-    // Close note area
-    mainCreateNote.style.display = 'none';
-    // Open note board
-    mainNoteBoard.style.display = 'block';
-    // Hide delete button
-    btnDelete.style.visibility = 'hidden';
+    closeNoteArea();
 })
 
 btnDelete.addEventListener('click', function() {
 
-    if (!noteEdited) {
-        // Close note area
-        mainCreateNote.style.display = 'none';
-        // Open note board
-        mainNoteBoard.style.display = 'block';
-        // Hide delete button
-        btnDelete.style.visibility = 'hidden';
-    } else {
+    if (noteEdited) {
         notes.splice(editedNoteIndex, 1);
         updateUI(notes);
         noteEdited = false;
-        // Close note area
-        mainCreateNote.style.display = 'none';
-        // Open note board
-        mainNoteBoard.style.display = 'block';
-        // Hide delete button
-        btnDelete.style.visibility = 'hidden';
     }
+
+    closeNoteArea();
+    // Clear text area
+    textArea.value = '';
+})
+
+btnCancel.addEventListener('click', function() {
+
+    closeNoteArea();
 })
 
 footerDate.textContent = new Date().getFullYear();
-
-// Update UI musi się odbywać za pomocą całej tablicy (ona musi być callbackiem), tak żeby potem można było zrobić update wpisać nowe notes i masz nowy UI
